d85065ee71354af6c714fdc649141e8645746815
Philipp Czora <philipp@czora.dev>
2020-06-22 21:07:15 +0200

    Specifies the key file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-
 mkdir -p ~/.ssh
 echo -e "$SSH_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 set -eu
-sh -c "rsync -avzP -e 'ssh -o StrictHostKeyChecking=no' $GITHUB_WORKSPACE/$SOURCE_PATH $REMOTE_USER@$HOST:$REMOTE_PATH"
+sh -c "rsync -avzP -e 'ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no' $GITHUB_WORKSPACE/$SOURCE_PATH $REMOTE_USER@$HOST:$REMOTE_PATH"
