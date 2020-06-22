ff835e97bcbd758721a9eb9f92ac3c37a21dbe47
Philipp Czora <philipp@czora.dev>
2020-06-22 21:01:25 +0200

    Removes log output of key

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,4 @@ mkdir -p ~/.ssh
 echo -e "$SSH_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 set -eu
-cat ~/.ssh/id_rsa 
 sh -c "rsync -avzP -e 'ssh -o StrictHostKeyChecking=no' $GITHUB_WORKSPACE/$SOURCE_PATH $REMOTE_USER@$HOST:$REMOTE_PATH"
