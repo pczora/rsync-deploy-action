c47dfab4916db69fbb03129204f1e9e28cc20cd8
Philipp Czora <philipp@czora.dev>
2020-06-22 21:13:22 +0200

    Use $HOME instead of ~

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-mkdir -p ~/.ssh
-echo -e "$SSH_KEY" > ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
+mkdir -p $HOME/.ssh
+echo -e "$SSH_KEY" > $HOME/.ssh/id_rsa
+chmod 600 $HOME/.ssh/id_rsa
 set -eu
-sh -c "rsync -avzP -e 'ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no' $GITHUB_WORKSPACE/$SOURCE_PATH $REMOTE_USER@$HOST:$REMOTE_PATH"
+sh -c "rsync -avzP -e 'ssh -i $HOME/.ssh/id_rsa -o StrictHostKeyChecking=no' $GITHUB_WORKSPACE/$SOURCE_PATH $REMOTE_USER@$HOST:$REMOTE_PATH"
