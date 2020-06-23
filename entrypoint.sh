b83bc219f282c6b8b2600b582a451061bd053370
Philipp Czora <philipp@czora.dev>
2020-06-23 09:00:49 +0200

    Adds input to add additional rsync parameters

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ mkdir -p .ssh
 echo -e "$SSH_KEY" > .ssh/id_rsa
 chmod 600 .ssh/id_rsa
 set -eu
-sh -c "rsync -avzP -e 'ssh -i .ssh/id_rsa -o StrictHostKeyChecking=no' $GITHUB_WORKSPACE/$SOURCE_PATH $REMOTE_USER@$HOST:$REMOTE_PATH"
+sh -c "rsync -avzP -e 'ssh -i .ssh/id_rsa -o StrictHostKeyChecking=no' $RSYNC_PARAMS $GITHUB_WORKSPACE/$SOURCE_PATH $REMOTE_USER@$HOST:$REMOTE_PATH"
